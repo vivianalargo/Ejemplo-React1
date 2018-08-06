89dcb79446eebccfbe5cebc647ae557214ab8d39
vivianalargo <vivilargo@gmail.com>
2018-08-06 15:21:48 -0500

    Primmer commit

---
+++ whearher-app/src/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import WeatherLocation from './components/WeatherLocation'
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Wheather App (Aplicación del clima)</h1>
+          <WeatherLocation/>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ whearher-app/src/components/WeatherLocation.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Location from './Location';
+import WeatherData from './WeatherData';
+
+
+
+
+const WeatherLocation = () => (
+<div>
+    <Location></Location>
+    <WeatherData></WeatherData>
+</div>
+);
+
+export default WeatherLocation;
